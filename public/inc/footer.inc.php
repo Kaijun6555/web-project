9d28ecaf5571f4f75c451f64e6918a77184ea654
DamSamm <samuelteoyj@gmail.com>
2025-04-06 00:02:27 +0800

    Update footer.inc.php

--- public/inc/footer.inc.php
+++ public/inc/footer.inc.php
@@ -1,3 +1,53 @@
-<footer class="container">
-    <p>Copyright &copy; 2025 Food Finder Pte. Ltd.</p>
+<footer class="bg-dark text-white pt-4 pb-2">
+    <div class="container">
+        <div class="row">
+            <!-- About Section -->
+            <div class="col-md-4 mb-3">
+                <h5>Food Finder</h5>
+                <p>Delivering delicious meals right to your door. Enjoy fast, reliable, and convenient food delivery from your favorite local restaurants.</p>
+            </div>
+            <!-- Quick Links -->
+            <div class="col-md-4 mb-3">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="/aboutus.php" class="text-white text-decoration-none">About Us</a></li>
+                    <li><a href="/user/restaurants.php" class="text-white text-decoration-none">Restaurants</a></li>
+                    <li><a href="/contact.php" class="text-white text-decoration-none">Contact</a></li>
+                    <li><a href="/faq.php" class="text-white text-decoration-none">FAQ</a></li>
+                </ul>
+            </div>
+            <!-- Social Media -->
+            <div class="col-md-4 mb-3">
+                <h5>Follow Us</h5>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white text-decoration-none">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white text-decoration-none">
+                            <i class="bi bi-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white text-decoration-none">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white text-decoration-none">
+                            <i class="bi bi-linkedin"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="bg-white">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="mb-0">&copy; <?= date("Y") ?> Food Finder. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
 </footer>
